361f16ce579019bcf6f6de600659dff625405457
Roope Salento <roope.salento@gmail.com>
2023-12-11 12:10:32 +0200

    update static page tests to also test contact us page rendering (#35)
    update static page tests to also test contact us page

--- app/tests/components/StaticPage/StaticPage.test.jsx
+++ app/tests/components/StaticPage/StaticPage.test.jsx
@@ -9,4 +9,26 @@ describe('Static Page component', () => {
 
     expect(screen.getByText('Static Page Title')).toBeInTheDocument();
   });
+
+  it('renders contact us page when handle is contact', () => {
+    const page = {title: 'Contact us title', body: null, handle: 'contact'};
+    render(<StaticPage page={page} />);
+
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Email')).toBeInTheDocument();
+    expect(screen.getByText('Phone Number')).toBeInTheDocument();
+    expect(screen.getByText('Comment')).toBeInTheDocument();
+  });
+
+  it('does not render contact us when handle is not contact', () => {
+    const page = {
+      title: 'Not Contact Us',
+      body: 'Hello World',
+      handle: 'not-contact',
+    };
+    render(<StaticPage page={page} />);
+
+    expect(screen.getByText('Not Contact Us')).toBeInTheDocument();
+    expect(screen.getByText('Hello World')).toBeInTheDocument();
+  });
 });
